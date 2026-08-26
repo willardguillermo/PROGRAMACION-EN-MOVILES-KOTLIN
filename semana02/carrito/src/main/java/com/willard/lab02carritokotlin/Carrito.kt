54976780caf0e1581d7dd0d1c4bb6d106108361d
willardguillermo <willardguillermo@gmail.com>
2026-08-26 13:51:40 -0500

    Proyecto inicial con menu del carrito

---
+++ semana02/carrito/src/main/java/com/willard/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.willard.lab02carritokotlin
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+}
