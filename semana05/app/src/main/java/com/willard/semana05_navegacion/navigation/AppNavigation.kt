92544fc7e930979d77c0c1b1ab68ef98e2ebc484
willardguillermo <willardguillermo@gmail.com>
2026-09-22 21:37:34 -0500

    Define Login como pantalla inicial en el NavHost

--- semana05/app/src/main/java/com/willard/semana05_navegacion/navigation/AppNavigation.kt
+++ semana05/app/src/main/java/com/willard/semana05_navegacion/navigation/AppNavigation.kt
@@ -9,40 +9,39 @@ import androidx.navigation.navArgument
 import com.willard.semana05_navegacion.screens.DetailScreen
 import com.willard.semana05_navegacion.screens.HomeScreen
 import com.willard.semana05_navegacion.screens.ListScreen
+import com.willard.semana05_navegacion.screens.LoginScreen
 import com.willard.semana05_navegacion.screens.ProfileScreen
 
 @Composable
 fun AppNavigation() {
-    // Crea y recuerda el controlador que maneja todo el back stack
     val navController = rememberNavController()
 
     NavHost(
         navController = navController,
-        startDestination = Screen.Home.route
+        startDestination = Screen.Login.route
     ) {
-        // Ruta simple: home
+        composable(Screen.Login.route) {
+            LoginScreen(navController)
+        }
         composable(Screen.Home.route) {
             HomeScreen(navController)
         }
-        // Ruta simple: list
         composable(Screen.List.route) {
             ListScreen(navController)
         }
-        // Ruta simple: profile
         composable(Screen.Profile.route) {
             ProfileScreen(navController)
         }
-        // Ruta con argumento tipado: detail/{itemId}
         composable(
             route = Screen.Detail.route,
             arguments = listOf(
                 navArgument(name = "itemId") {
                     type = NavType.IntType
-                    defaultValue = 0
+                    defaultValue = 1
                 }
             )
         ) { backStackEntry ->
-            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 1
             DetailScreen(navController, itemId)
         }
     }
